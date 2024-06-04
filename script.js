64554f66958ef58c4ba314939fecc6dd16ce5825
Allan <crokdiab@gmail.com>
2024-06-04 18:03:34 +0200

    add boostrap

--- script.js
+++
@@ -1,8 +0,0 @@
-// -------- NAVBAR --------
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-});
